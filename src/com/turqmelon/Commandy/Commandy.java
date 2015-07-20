9292ca68db9767d38418332395a25843233aefc8
Turqmelon <devonthome@gmail.com>
2015-07-20 10:43:34 -0700

    Initial commit

---
+++ src/com/turqmelon/Commandy/Commandy.java
@@ -0,0 +1,14 @@
+package com.turqmelon.Commandy;
+
+/**
+ * ***********************************************************************
+ * Copyright Turqmelon (c) 2015. All Rights Reserved.
+ * Any code contained within this document, and any associated APIs with similar branding
+ * are the sole property of Turqmelon. Distribution, reproduction, taking snippets, or
+ * claiming any contents as your own will break the terms of the liscense, and void any
+ * agreements with you, the third party.
+ * Thanks.
+ * ************************************************************************
+ */
+public class Commandy {
+}
